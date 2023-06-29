ea63b50d70adf9ce0fbdd88dd1b1eab4331fbbc8
Kelsea Blackwell <kelsea@darklightventures.com>
2023-06-28 21:56:48 -0400

    Basic Implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,52 @@
 /*
- *  Kelsea Blackwell (c) 2021
+ *  Kelsea Blackwell (c) 2023
  *  See LICENSE for licensing information.
  */
 
 use std::collections::VecDeque;
+use std::process;
+use tracing::error;
 
+trait EmergencyBrake {
+    fn sample(&mut self, sample: bool);
+    fn trigger(&self) -> bool;
+}
 
-pub struct EBrake<T> {
-    data: VecDeque<T>,
+#[derive(Clone, Debug, Default)]
+pub struct EBrake {
+    data: VecDeque<bool>,
     failures: usize,
     samples: usize,
     successes: usize,
     tolerance: usize,
 }
 
+
+impl EmergencyBrake for EBrake {
+    fn sample(&mut self, sample: bool) {
+        if self.data.len() == self.samples {
+            match self.data.pop_front() {
+                Some(true) => self.successes -= 1,
+                Some(false) => self.failures -= 1,
+                None => {},
+            }
+        }
+        self.data.push_back(sample);
+    }
+
+    fn trigger(&self) -> bool {
+        if self.data.len() < self.samples {
+            return false;
+        }
+
+        if self.failures > self.tolerance {
+            error!("Emergency brake triggered!");
+            process::abort();
+        }
+
+        false
+    }
+}
 
 
 #[cfg(test)]
---
+++ src/test.rs
@@ -0,0 +1,10 @@
+/*
+ *  Kelsea Blackwell (c) 2023
+ *  See LICENSE for licensing information.
+ */
+
+use super::*;
+
+#[test]
+fn it_should_return_false_when_not_triggered() {
+}
